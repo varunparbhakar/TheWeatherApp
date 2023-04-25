c26d5530d9de1cd2707880c78ebad774a54b3931
seburoh <seburoh@outlook.com>
2023-04-25 16:05:33 -0700

    making bottom nav not crash

--- app/src/main/java/edu/uw/tcss450/varpar/weatherapp/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/varpar/weatherapp/MainActivity.java
@@ -26,9 +26,9 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_home, R.id.navigation_weather,
                 R.id.navigation_contacts, R.id.navigation_chat)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+//        NavigationUI.setupWithNavController(navView, navController);
 
     }
 }
