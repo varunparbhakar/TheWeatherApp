2d0f2af576940dad49ab2021c99688aa9d0d6aca
Nattypat <kfcxingers@gmail.com>
2023-04-29 14:17:36 -0700

    Chat: Card, List, Message - Started

--- app/src/main/java/edu/uw/tcss450/varpar/weatherapp/chat/ChatListViewModel.java
+++ app/src/main/java/edu/uw/tcss450/varpar/weatherapp/chat/ChatListViewModel.java
@@ -1,2 +1,39 @@
-package edu.uw.tcss450.varpar.weatherapp.chat;public class ChatListViewModel {
+package edu.uw.tcss450.varpar.weatherapp.chat;
+
+import static com.android.volley.DefaultRetryPolicy.*;
+
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.IntFunction;
+
+import edu.uw.tcss450.varpar.weatherapp.R;
+
+public class ChatListViewModel extends AndroidViewModel {
+
+    public ChatListViewModel(@NonNull Application application) {
+        super(application);
+    }
+
 }
+
