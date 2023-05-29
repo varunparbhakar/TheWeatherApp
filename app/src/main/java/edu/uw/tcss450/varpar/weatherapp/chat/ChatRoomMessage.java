e068b14d69da66dc50451a3643b74b61c387a4ab
Nattypat <kfcxingers@gmail.com>
2023-05-29 13:20:56 -0700

    ChatRoomMessage -> createFromJsonString

--- app/src/main/java/edu/uw/tcss450/varpar/weatherapp/chat/ChatRoomMessage.java
+++ app/src/main/java/edu/uw/tcss450/varpar/weatherapp/chat/ChatRoomMessage.java
@@ -4,6 +4,9 @@ import android.icu.text.SimpleDateFormat;
 
 import androidx.annotation.Nullable;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.Serializable;
 import java.util.Date;
 
@@ -28,6 +31,22 @@ public class ChatRoomMessage implements Serializable {
         this.mMessageBody = messageBody;
         this.mSender = user;
         this.mTimestamp = new SimpleDateFormat("HH.mm.ss").format(new Date());
+    }
+
+    /**
+     * Static factory method to turn a properly formatted JSON String into a
+     * ChatMessage object.
+     * @param cmAsJson the String to be parsed into a ChatMessage Object.
+     * @return a ChatMessage Object with the details contained in the JSON String.
+     * @throws JSONException when cmAsString cannot be parsed into a ChatMessage.
+     */
+    public static ChatRoomMessage createFromJsonString(final String cmAsJson) throws JSONException {
+        final JSONObject msg = new JSONObject(cmAsJson);
+        return new ChatRoomMessage(
+                msg.getInt("messageid"),
+                msg.getString("message"),
+                msg.getString("email")
+        );
     }
 
     public int getMessageId() {
