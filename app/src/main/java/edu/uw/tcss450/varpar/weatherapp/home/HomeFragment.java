cfe54888ec855cc940700c571a88add610ffffa0
Deep Singh <deepsingh.khalsa25@gmail.com>
2023-06-03 23:13:26 -0700

    current loc works? on home page

--- app/src/main/java/edu/uw/tcss450/varpar/weatherapp/home/HomeFragment.java
+++ app/src/main/java/edu/uw/tcss450/varpar/weatherapp/home/HomeFragment.java
@@ -1,7 +1,13 @@
 package edu.uw.tcss450.varpar.weatherapp.home;
 
+import static android.content.Context.LOCATION_SERVICE;
+
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -21,15 +27,22 @@ import com.android.volley.RequestQueue;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 
@@ -47,6 +60,8 @@ public class HomeFragment extends Fragment {
     private FriendReqRVAdapter friendReqRVAdapter;
     private RecyclerView FriendReqRV;
     private UserInfoViewModel mUserModel;
+    private String currentCity;
+    private LocationManager locationManager;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -62,6 +77,12 @@ public class HomeFragment extends Fragment {
         if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_CODE);
         }
+        locationManager = (LocationManager) getContext().getSystemService(LOCATION_SERVICE);
+        Location location = getLastKnownLocation();
+        if (location == null) {
+            requestLocationUpdates();
+        }
+        currentCity = getCityName(location.getLongitude(), location.getLatitude());
 
         getWeatherInfo();
 
@@ -73,9 +94,93 @@ public class HomeFragment extends Fragment {
         // Get the friend requests and display them
         getFriendsRequests();
     }
+    private Location getLastKnownLocation() {
+        requestLocationUpdates();
+        LocationManager mLocationManager = (LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
+        List<String> providers = mLocationManager.getProviders(true);
+        Location bestLocation = null;
+        for (String provider : providers) {
+            requestLocationUpdates();
+            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                //ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_CODE);
+            }
+            Location l = mLocationManager.getLastKnownLocation(provider);
+            Log.d("l", "l is " + l);
+            if (l == null) {
+                continue;
+            }
+            if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
+                // Found best last known location: %s", l);
+                bestLocation = l;
+            }
+        }
+        return bestLocation;
+    }
+
+    private void requestLocationUpdates() {
+        LocationRequest mLocationRequest = LocationRequest.create();
+        mLocationRequest.setInterval(60000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        LocationCallback mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    if (location != null) {
+                        //TODO: UI updates.
+                    }
+                }
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_CODE);
+        }
+        LocationServices.getFusedLocationProviderClient(getActivity()).requestLocationUpdates(mLocationRequest, mLocationCallback, null);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            locationManager = (LocationManager) getContext().getSystemService(LOCATION_SERVICE);
+            Location location = getLastKnownLocation();
+            if (location == null) {
+                requestLocationUpdates();
+            }
+            currentCity = getCityName(location.getLongitude(), location.getLatitude());
+            getWeatherInfo(); //this is all to get current location so the app displays it when opned
+            //getLastKnownLocation();
+        } else {
+            Log.wtf("uwu", "permission request failed");
+        }
+    }
+
+    public String getCityName(double longitude, double latitide) {
+        String cityName = "Not Found";
+        Geocoder gcd = new Geocoder(getActivity().getBaseContext(), Locale.getDefault());
+        try {
+            List<Address> addressList = gcd.getFromLocation(latitide,longitude,10);
+            for(Address adr : addressList) {
+                if(adr!=null) {
+                    String city = adr.getLocality();
+                    if(city!=null && !city.equals("")) {
+                        cityName = city;
+                    } else {
+                        Log.d("Tag", "City not found");
+                        //Toast.makeText(getActivity(),"User city not found...", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return cityName;
+    }
 
     private void getWeatherInfo() {
-        String URL = "http://api.weatherapi.com/v1/forecast.json?key=9953bd3e0e9448fba21212344231405 &q=Tacoma&days=5&aqi=no&alerts=no";
+        String URL = getText(R.string.url) + "weather?zipcode=" + currentCity;
         RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, URL, null,
